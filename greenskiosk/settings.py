5da4fb90340ddcefcc9bc037332ce6b3c8796c61
Hellenivy <hellenakinyiivy@gmail.com>
2020-09-05 23:32:24 +0300

    work

--- greenskiosk/settings.py
+++ greenskiosk/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalogue',
+    'kiosk',
+    'shopping',
+    'shipping',
+    
 ]
 
 MIDDLEWARE = [
